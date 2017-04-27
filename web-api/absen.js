f5193fe8dad17fb6a3e176bda20c171c49293de2
syuria27 <syukron.tkj2@gmail.com>
2017-04-27 21:32:53 +0700

    Edit query using table view

--- web-api/absen.js
+++ web-api/absen.js
@@ -15,22 +15,11 @@ ABSEN_ROUTER.prototype.handleRoutes = function (router, pool) {
         var data = {
             error: true,
             error_msg: "",
-            user: {},
             absen: []
         };
 
-        var query = `SELECT a.kode_absen, 
-	    			COALESCE(u.nama_spg, ' ') as nama_spg,
-	    			COALESCE(u.nama_toko, ' ') as nama_toko,
-	    			COALESCE(u.depot, ' ') as depot,
-					DATE_FORMAT(a.tanggal, '%d-%m-%Y') as tanggal,
-					CAST(COALESCE(a.jam_masuk, ' ') as CHAR) as jam_masuk,
-					COALESCE(a.lokasi_masuk, ' ') as lokasi_masuk,
-					CAST(COALESCE(a.jam_pulang, ' ') as CHAR) as jam_pulang,
-					COALESCE(a.lokasi_pulang, ' ') as lokasi_pulang
-					FROM absen a LEFT JOIN user u ON a.kode_spg = u.kode_spg
-        			WHERE a.kode_spg = ? AND MONTH(tanggal) = ? AND YEAR(tanggal) = ?`;
-
+        var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl
+                    FROM Absen_Report WHERE kode_spg = ? AND MONTH(tanggal) = ? AND YEAR(tanggal) = ?`;
         var table = [req.params.kode_spg, req.params.bulan, req.params.tahun];
         query = mysql.format(query, table);
         pool.getConnection(function (err, connection) {
@@ -45,17 +34,10 @@ ABSEN_ROUTER.prototype.handleRoutes = function (router, pool) {
                     if (rows.length > 0) {
                         data.error = false;
                         data.error_msg = 'Success..';
-                        data.user = {
-                            kode_spg: rows[0].kode_spg,
-                            nama_spg: rows[0].nama_spg,
-                            nama_toko: rows[0].nama_toko,
-                            depot: rows[0].depot
-                        };
-
                         rows.forEach(function (absen) {
                             var abs = {
                                 kode_absen: absen.kode_absen,
-                                tanggal: absen.tanggal,
+                                tanggal: absen.tgl,
                                 jam_masuk: absen.jam_masuk,
                                 lokasi_masuk: absen.lokasi_masuk,
                                 jam_pulang: absen.jam_pulang,
@@ -84,33 +66,17 @@ ABSEN_ROUTER.prototype.handleRoutes = function (router, pool) {
         };
 
         if (req.params.depot === "ADMIN") {
-            var query = `SELECT a.kode_absen, 
-	    			COALESCE(u.nama_spg, ' ') as nama_spg,
-	    			COALESCE(u.nama_toko, ' ') as nama_toko,
-	    			COALESCE(u.depot, ' ') as depot,
-					DATE_FORMAT(a.tanggal, '%d-%m-%Y') as tanggal,
-					CAST(COALESCE(a.jam_masuk, ' ') as CHAR) as jam_masuk,
-					COALESCE(a.lokasi_masuk, ' ') as lokasi_masuk,
-					CAST(COALESCE(a.jam_pulang, ' ') as CHAR) as jam_pulang,
-					COALESCE(a.lokasi_pulang, ' ') as lokasi_pulang
-					FROM absen a LEFT JOIN user u ON a.kode_spg = u.kode_spg WHERE tanggal = ?`;
+            var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl 
+                        FROM Absen_Report WHERE tanggal = ?`;
             var table = [req.params.tanggal];
         } else {
-            var query = `SELECT a.kode_absen, 
-	    			COALESCE(u.nama_spg, ' ') as nama_spg,
-	    			COALESCE(u.nama_toko, ' ') as nama_toko,
-	    			COALESCE(u.depot, ' ') as depot,
-					DATE_FORMAT(a.tanggal, '%d-%m-%Y') as tanggal,
-					CAST(COALESCE(a.jam_masuk, ' ') as CHAR) as jam_masuk,
-					COALESCE(a.lokasi_masuk, ' ') as lokasi_masuk,
-					CAST(COALESCE(a.jam_pulang, ' ') as CHAR) as jam_pulang,
-					COALESCE(a.lokasi_pulang, ' ') as lokasi_pulang
-					FROM absen a LEFT JOIN user u ON a.kode_spg = u.kode_spg
-	        		WHERE depot = ? AND tanggal = ?`;
+            var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl 
+                        FROM Absen_Report WHERE depot = ? AND tanggal = ?`;
             var table = [req.params.depot, req.params.tanggal];
         }
 
         query = mysql.format(query, table);
+        console.log(query);
         pool.getConnection(function (err, connection) {
             connection.query(query, function (err, rows) {
                 connection.release();
@@ -123,8 +89,20 @@ ABSEN_ROUTER.prototype.handleRoutes = function (router, pool) {
                     if (rows.length > 0) {
                         data.error = false;
                         data.error_msg = 'Success..';
-                        data.absen = rows;
-                        res.status(200);
+                        rows.forEach(function (absen) {
+                            var abs = {
+                                kode_absen: absen.kode_absen,
+                                nama_spg: absen.nama_spg,
+                                nama_toko: absen.nama_spg,
+                                depot: absen.depot,
+                                tanggal: absen.tgl,
+                                jam_masuk: absen.jam_masuk,
+                                lokasi_masuk: absen.lokasi_masuk,
+                                jam_pulang: absen.jam_pulang,
+                                lokasi_pulang: absen.lokasi_pulang
+                            }
+                            data.absen.push(abs);
+                        });res.status(200);
                         res.json(data);
                     } else {
                         data["error_msg"] = 'No Absen Found..';
--- web-api/product.js
+++ web-api/product.js
@@ -40,6 +40,79 @@ PRODUCT_ROUTER.prototype.handleRoutes = function (router, pool) {
             });
         });
     });
+
+    router.get("/product/user/:kode_spg/:bulan/:tahun", function (req, res) {
+        var data = {
+            error: true,
+            error_msg: ""
+        };
+
+        var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl
+                    FROM Daily_Product_Report WHERE kode_spg = ? AND MONTH(tanggal) = ? AND YEAR(tanggal) = ?`;
+        var table = [req.params.kode_spg, req.params.bulan, req.params.tahun];
+        query = mysql.format(query,table);
+        pool.getConnection(function (err, connection) {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    res.status(500);
+                    data.error_msg = "Error executing MySQL query";
+                    res.json(data);
+                } else {
+                    if (rows.length != 0) {
+                        res.status(200);
+                        data.error = false;
+                        data.error_msg = 'Success..';
+                        data.products = rows;
+                        res.json(data);
+                    } else {
+                        res.status(404);
+                        data.error_msg = 'No product Found..';
+                        res.json(data);
+                    }
+                }
+            });
+        });
+    });
+
+    router.get("/product/depot/:depot/:tanggal", function (req, res) {
+        var data = {
+            error: true,
+            error_msg: ""
+        };
+
+        if (req.params.depot === "ADMIN") {
+            var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl FROM Daily_Product_Report WHERE tanggal = ?`;
+            var table = [req.params.tanggal];
+        } else {
+            var query = `SELECT *, DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl FROM Daily_Product_Report WHERE tanggal = ? AND depot = ?`;
+            var table = [req.params.tanggal, req.params.depot];
+        }
+        query = mysql.format(query,table);
+        pool.getConnection(function (err, connection) {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                    data.error_msg = "Error executing MySQL query";
+                    res.json(data);
+                } else {
+                    if (rows.length != 0) {
+                        res.status(200);
+                        data.error = false;
+                        data.error_msg = 'Success..';
+                        data.products = rows;
+                        res.json(data);
+                    } else {
+                        res.status(404);
+                        data.error_msg = 'No product Found..';
+                        res.json(data);
+                    }
+                }
+            });
+        });
+    });
 }
 
 module.exports = PRODUCT_ROUTER;
