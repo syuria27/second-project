3720ca17b888fed410aa8346422bed6f25b6d142
syuria27 <syukron.tkj2@gmail.com>
2017-05-01 18:29:17 +0700

    add monthly product

--- web-api/product.js
+++ web-api/product.js
@@ -78,7 +78,7 @@ PRODUCT_ROUTER.prototype.handleRoutes = function (router, pool) {
         });
     });
 
-    router.get("/product/depot/:depot/:tanggal", function (req, res) {
+    router.get("/product/daily/:depot/:tanggal", function (req, res) {
         var data = {
             error: true,
             error_msg: ""
@@ -96,6 +96,49 @@ PRODUCT_ROUTER.prototype.handleRoutes = function (router, pool) {
                         DATE_FORMAT(tanggal, '%d-%m-%Y') as tgl
                         FROM Daily_Product_Report WHERE tanggal = ? AND depot = ?`;
             var table = [req.params.tanggal, req.params.depot];
+        }
+        query = mysql.format(query,table);
+        pool.getConnection(function (err, connection) {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                    data.error_msg = "Error executing MySQL query";
+                    res.json(data);
+                } else {
+                    if (rows.length != 0) {
+                        res.status(200);
+                        data.error = false;
+                        data.error_msg = 'Success..';
+                        data.products = rows;
+                        res.json(data);
+                    } else {
+                        res.status(404);
+                        data.error_msg = 'No product Found..';
+                        res.json(data);
+                    }
+                }
+            });
+        });
+    });
+
+    router.get("/product/monthly/:depot/:bulan/:tahun", function (req, res) {
+        var data = {
+            error: true,
+            error_msg: ""
+        };
+
+        if (req.params.depot === "ADMIN") {
+            var query = `SELECT *, concat('Rp. ',format(ccm,0)) as jml_ccm,
+                        concat('Rp. ',format(rm,0)) as jml_rm
+                        FROM Monthly_Product_Report WHERE bulan = ? AND tahun = ?`;
+            var table = [req.params.bulan, req.params.tahun];
+        } else {
+            var query = `SELECT *, concat('Rp. ',format(ccm,0)) as jml_ccm,
+                        concat('Rp. ',format(rm,0)) as jml_rm
+                        FROM Monthly_Product_Report WHERE bulan = ? AND tahun = ? AND depot = ?`;
+            var table = [req.params.bulan, req.params.tahun, req.params.depot];
         }
         query = mysql.format(query,table);
         pool.getConnection(function (err, connection) {
